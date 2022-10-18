2c0201321482bb94ecc75f5d3ef8107be0bef508
André Hopf <andre.hopf@gmail.com>
2022-10-18 11:52:06 -0300

    Adiciona verificação se parametros sao arrays

--- ex3/app.js
+++ ex3/app.js
@@ -1,6 +1,12 @@
 const concatena = (array1, array2) => {
-  const novoArray = [...array1, ...array2];
-  console.log(novoArray);
+  if (Array.isArray(array1) && Array.isArray(array2)) {
+    const novoArray = [...array1, ...array2];
+    console.log(novoArray);
+  } else {
+    console.log('Ambos parâmetros devem ser arrays!');
+  }
 };
 
 concatena([1, 2, 3], [4, 5, 6]);
+concatena([1], 2);
+concatena(1, [2]);

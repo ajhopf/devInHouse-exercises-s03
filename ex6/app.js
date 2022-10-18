86f48ae6d0ae452d165f0ea75cf7809a176a89a4
André Hopf <andre.hopf@gmail.com>
2022-10-18 14:41:48 -0300

    Exercício 6 concluído

---
+++ ex6/app.js
@@ -0,0 +1,23 @@
+const verifyPalindrome = string => {
+  console.log('\nOriginal string: ', string);
+
+  //remove whitespaces and makes all letters lower case
+  const formattedString = string.split(' ').join('').toLowerCase();
+  console.log('String to lower case and no whitespaces: ', formattedString);
+
+  const arrayFromString = formattedString.split('');
+
+  const reversedString = arrayFromString.reverse().join('');
+
+  console.log('Strings to compare: ', formattedString, ' / ', reversedString);
+  console.log(`Is it a palindrome? ${formattedString == reversedString}`);
+  return formattedString == reversedString;
+};
+
+verifyPalindrome('Hello my friend');
+verifyPalindrome('Andre');
+verifyPalindrome('luz azul');
+verifyPalindrome('osso');
+verifyPalindrome('Rats live on no evil star');
+verifyPalindrome('Ana');
+verifyPalindrome('Mr Owl ate my metal worm');
